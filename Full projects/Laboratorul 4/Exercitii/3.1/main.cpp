bda9d9416c8d928783086aadb8a8348cc4283d1a
Andrei Dana <evohuntmaster@gmail.com>
2017-11-09 23:12:22 +0200

    Add files via upload

---
+++ Full projects/Laboratorul 4/Exercitii/3.1/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class A {
+    int i;
+    string s;
+
+    public:
+        A (int i1, string s1): i(i1), s(s1) {} // notati modul de apel ptr i si s din A !
+        int getI() {
+
+            return this -> i;
+        }
+        string getS() {
+
+            return this -> s;
+
+        }
+
+};
+
+class B {
+    A a;
+    string s;
+
+    public:
+        B(int i2,string s2): a(i2,s2), s("init value for s in A") {} // notati modul de apel ptr constructor
+        string getS() {
+
+            return this -> s;
+
+        }
+        A getA() {
+
+            return this -> a;
+
+        }
+
+};
+
+int main()
+{
+    B b (5, "init value for s in class A");
+    cout << "B s:" << b.getS() << '\n';
+    cout << "A s:" << b.getA().getI() << " i:" << b.getA().getS() << '\n';
+
+    return 0;
+}

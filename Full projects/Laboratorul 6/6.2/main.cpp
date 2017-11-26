e3a016f49ca06898f15e4bebc8252fa152d49f36
Evohunt <evohuntmaster@gmail.com>
2017-11-26 15:04:15 +0200

    Added 6.2

---
+++ Full projects/Laboratorul 6/6.2/main.cpp
@@ -0,0 +1,15 @@
+/// T.6.2. Rescrieti clasa Operatii de la T.6.1. astfel incit sa existe o metoda statica, supraincarcată pentru
+/// calculul modulului unui double respectiv al unui număr complex. Analog, o metodă statică pentru
+/// afișarea numărului real sau complex.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+
+    return 0;
+}

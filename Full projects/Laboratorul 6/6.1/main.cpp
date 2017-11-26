b5002c348ef00f6a5d2967a89062ae777648d53f
Evohunt <evohuntmaster@gmail.com>
2017-11-26 11:21:26 +0200

    Added Laboratorul 6

---
+++ Full projects/Laboratorul 6/6.1/main.cpp
@@ -0,0 +1,14 @@
+/// T.6.1 Scrieti o clasa Operatii care va avea 2 constructori cu parametrii un double respectiv o clasa
+/// NumarComplex. Definiti clasa NumarComplex. Clasa Operatii implementeaza o metodă pentru calculul
+/// modulului numartului transmis ca parametru, doule sau numar complex. Clasa Operatii va avea o
+/// metoda afisare, supraîncărcată, care afișează un număr real sau complex.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}

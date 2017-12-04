c978c0e8ae4dd1ec5b18a12598873a229e7f1c98
Evohunt <evohuntmaster@gmail.com>
2017-12-04 19:50:01 +0200

    Added 6.3

---
+++ Full projects/Laboratorul 6/6.3/main.cpp
@@ -0,0 +1,14 @@
+/// T.6.3. Scrieti o clasa Operatii care va avea 2 constructori cu parametrii doi double respectiv două clase
+/// NumarComplex. Clasa Operatii implementeaza metode supraîncărcate pentru calculul sumei, diferenței,
+/// produsului și împărțirii a două numere, double sau NumarComplex. Metodele folosite vor fi
+/// supraîncărcate (același nume).
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
